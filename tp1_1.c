7569975feafa0f3c4a3ee4388a1476a4ba9c4ca1
Ramón Alejandro Ruiz <alexestelar@yahoo.com.ar>
2023-03-23 16:21:45 -0300

    punteros

--- tp1_1.c
+++ tp1_1.c
@@ -1,6 +1,18 @@
 #include <stdio.h>
     int main()
     {
+        int var = 5;
+        int *p = &var;
+
+    //3a)
         printf("Hola mundo");
+
+    //3e)
+        printf("\nContenido del puntero: %d", *p);
+        printf("\nDirección de memoria almaceneda por el puntero: %p", p);
+        printf("\nDirección de memoria de la variable: %p", &var);
+        printf("\nDirección de memoria del puntero: %p", &p);
+        printf("\nTamaño de memoria utilizado por la variable: %d", sizeof(var));
+
         return 0;
     }
